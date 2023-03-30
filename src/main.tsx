232322d89d884fe05453aa42713b419e4d850e4a
ETMC\cmaturana <CMaturana@telemedicineclinic.com>
2023-03-30 12:32:29 +0200

    a

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { createRoot } from "react-dom/client";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import './index.css'
 
 import App from './App'
 import DropComponent from "./drop"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App />
+  },
+  {
+    path: "drop",
+    element: <DropComponent />,
+  },
+]);
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/drop" element={<DropComponent />} />
-      </Routes>
-    </BrowserRouter>
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
