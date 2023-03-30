d3ef520c150208727ba2aeba3708201064463a2f
ETMC\cmaturana <CMaturana@telemedicineclinic.com>
2023-03-30 12:43:17 +0200

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,6 @@ const App: React.FC = () => {
     <>
       <Container id="container-1">
         <h2>Drag</h2>
-        <div>
-          <nav>
-            <ul className="flex justify-center mb-2">
-              <li className="mx-4">
-                <Link to="/" className="text-gray-800 hover:text-gray-600 font-medium">drag</Link>
-              </li>
-              <li className="mx-4">
-                <Link to="/drop" className="text-gray-800 hover:text-gray-600 font-medium">drop</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
         {
           widgets.map((element) => (
             <Item parent="container-1" key={element.name} id={element.name} content={element.widget} />
